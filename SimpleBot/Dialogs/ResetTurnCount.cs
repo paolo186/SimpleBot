40f5d1a4621656dbf7c445b74f36c4444a440f44
Bryan Paolo Santos <v-brsa@microsoft.com>
2019-05-03 15:29:22 -0700

    User can now choose a turn count number; if input is invalid, user has 3 attempts before ending the conversation and retaining previous turn count

--- SimpleBot/Dialogs/ResetTurnCount.cs
+++ SimpleBot/Dialogs/ResetTurnCount.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Internals;
 using Microsoft.Bot.Connector;
 using Microsoft.Bot.Connector.Teams;
 using System;
@@ -14,6 +15,7 @@ namespace SimpleBot.Dialogs
     {
 
         private int count;
+        private int attempts = 3;
 
         public ResetTurnCount(int count)
         {
@@ -40,17 +42,45 @@ namespace SimpleBot.Dialogs
 
             if (confirm)
             {
-                this.count = 1;
-                await context.PostAsync("Reset count to 1.");
+                //this.count = 1;
+                //await context.PostAsync("Reset count to 1.");
+
+                await context.PostAsync("Sure, I can help with that. Choose a whole number > 0 (e.g. '1', '100')");
+                context.Wait(ChooseNewTurn);
 
             }
             else
             {
-                // current count was previously updated. Subtract 1 to get actual current count
-                await context.PostAsync($"Did not reset count. (count={this.count - 1}) ");
+                await context.PostAsync($"Did not reset turn count. (next count={this.count}) ");
+                context.Done(this.count);
             }
-            context.Done(this.count);
+            //context.Done(this.count);
 
+
+        }
+
+        private async Task ChooseNewTurn(IDialogContext context, IAwaitable<IMessageActivity> result)
+        {
+            var message = await result;
+            int age;
+
+
+
+            if (Int32.TryParse(message.Text, out age) && (age > 0))
+            {
+                await context.PostAsync($"Changed turn count to {age}.");
+                context.Done(age);
+            }
+            else if ((--attempts) <= 0)
+            {
+
+                context.Fail(new TooManyAttemptsException("Message was not a valid whole number"));
+            }
+            else
+            { 
+                await context.PostAsync("I didn't get that. I'm looking for whole numbers greater than 0 (e.g. '1', '30')");
+                context.Wait(ChooseNewTurn);
+            }
 
         }
     }
--- SimpleBot/Dialogs/RootDialog.cs
+++ SimpleBot/Dialogs/RootDialog.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
 using Microsoft.Bot.Connector.Teams;
+using System.Text;
 
 namespace SimpleBot.Dialogs
 {
@@ -27,7 +28,9 @@ namespace SimpleBot.Dialogs
 
             if (message.ToLower().Contains("reset"))
             {
+    
                 context.Call(new ResetTurnCount(this.turnCount), AfterResetTurnCount);
+
             }
             else
             {
@@ -42,8 +45,23 @@ namespace SimpleBot.Dialogs
 
         private async Task AfterResetTurnCount(IDialogContext context, IAwaitable<int> result)
         {
-            this.turnCount = await result;
-            context.Wait(MessageReceivedAsync);
+            try
+            {
+                var number = await result;
+                this.turnCount = number;
+            }
+            catch (TooManyAttemptsException e)
+            {
+                StringBuilder sb = new StringBuilder($"I'm sorry, I'm having issues understanding you. {e.Message}.");
+                sb.Append($"\nDid not reset turn count. (next count={this.turnCount})");
+                await context.PostAsync(sb.ToString());
+                
+            } finally
+            {
+                context.Wait(MessageReceivedAsync);
+            }
+
+            
 
         }
 
